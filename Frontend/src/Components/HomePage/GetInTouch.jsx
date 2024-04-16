f434d421843be2f85b39e3f646b911592e198f1a
Priyank Jain <priyankjohn67@gmail.com>
2024-04-16 23:11:03 +0530

    Co-authored-by: Prathmesh Takalkar <Ptmesh@users.noreply.github.com> Co-authored-by: Anuja Sakulkar <anujasakulkar.mitwpu@gmail.com> Co-authored-by: Gaurav Untawale <Gaurav-1032201414@users.noreply.github.com>

---
+++ Frontend/src/Components/HomePage/GetInTouch.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(
+      rgba(255, 255, 255, 0.5),
+      rgba(255, 255, 255, 0.5)
+    ),
+    url("") center;
+  background-size: cover;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Wrapper = styled.div`
+  padding: 20px;
+  width: 69%;
+  background-color: white;
+`;
+const Form = styled.form`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 20px 10px 0px 0px;
+  padding: 10px;
+`;
+
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 15px 20px;
+  background-color: teal;
+  color: white;
+  cursor: pointer;
+  text-align:center;
+`;
+const Title = styled.h1`
+  font-size: 24px;
+  font-weight: 300;
+text-align:center;
+`;
+
+const GetInTouch = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>GET IN TOUCH</Title>
+        <Form>
+          <Input placeholder="First Name" />
+          <Input placeholder="Email" />
+          <Input placeholder="Subject" />
+          <Input placeholder="Phone Number" />
+          <Input placeholder="Message" />
+
+  <Button>CREATE MY ACCOUNT</Button>
+        </Form>
+
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default GetInTouch;
--- Frontend/src/Pages/Landing Page/Home.jsx
+++ Frontend/src/Pages/Landing Page/Home.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import FooterHome from "../../Components/HomePage/FooterHome";
 import NavbarHome from "../../Components/HomePage/NavbarHome";
+import GeniueSection from "../../Components/HomePage/GeniueSection";
+import TodoAgenda from "../../Components/HomePage/TodoAgenda";
+import Ecosystem from "../../Components/HomePage/Ecosystem";
+import GetInTouch from "../../Components/HomePage/GetInTouch";
+import FooterHome from "../../Components/HomePage/FooterHome";
 
 const Home = () => {
   return (
     <>
       <NavbarHome />
+      <GeniueSection />
+      <TodoAgenda />
+      <Ecosystem />
+      <GetInTouch />
       <FooterHome />
+
     </>
   );
 };
