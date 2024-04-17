f6fcff676c3786bb83a3fe5e472c9765645da1db
Priyank Jain <priyankjohn67@gmail.com>
2024-04-17 22:38:27 +0530

    merge

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,24 @@
 import Login from "./Pages/Software/Login";
 
+import PrivacyPolicy from "./Pages/Landing Page/PrivacyPolicy";
+import AboutUs from "./Pages/Landing Page/AboutUs";
+import Home from "./Pages/Landing Page/Home";
+import FooterHome from "./Components/HomePage/FooterHome";
+import GeniueSection from "./Components/HomePage/GeniueSection";
+import TodoAgenda from "./Components/HomePage/TodoAgenda";
+import GetInTouch from "./Components/HomePage/GetInTouch";
+import Login from "./Pages/Landing Page/Login";
+
+
 function App() {
   return (
     <>
+
       <Login />
+
+      <Home />
+
+
     </>
   );
 }
