f84604aadf2f441a31d06392043721fd09965f2a
Priyank Jain <priyankjohn67@gmail.com>
2024-04-17 23:53:59 +0530

    register page
    created register page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,12 @@
 import Login from "./Pages/Software/Login";
+import Register from "./Pages/Software/Register.jsx";
+
 
 function App() {
   return (
     <>
       <Login />
+      {/* <Register /> */}
     </>
   );
 }
