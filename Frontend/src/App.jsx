fa4361fb405b21f67f5a2115046cd5a38c3a12ec
Prathmesh Takalkar <takalkarprathmesh@gmail.com>
2024-04-18 22:42:23 +0530

    Meet The Team

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,33 +1,9 @@
-<<<<<<< HEAD
 import MeetTheTeam from "./Pages/Landing Page/MeetTheTeam";
 function App() {
   return (
     <>
       <MeetTheTeam />
       {/* <Login /> */}
-=======
-import Login from "./Pages/Software/Login";
-
-import PrivacyPolicy from "./Pages/Landing Page/PrivacyPolicy";
-import AboutUs from "./Pages/Landing Page/AboutUs";
-import Home from "./Pages/Landing Page/Home";
-import FooterHome from "./Components/HomePage/FooterHome";
-import GeniueSection from "./Components/HomePage/GeniueSection";
-import TodoAgenda from "./Components/HomePage/TodoAgenda";
-import GetInTouch from "./Components/HomePage/GetInTouch";
-import Login from "./Pages/Landing Page/Login";
-
-
-function App() {
-  return (
-    <>
-
-      <Login />
-
-      <Home />
-
-
->>>>>>> f6fcff676c3786bb83a3fe5e472c9765645da1db
     </>
   );
 }
