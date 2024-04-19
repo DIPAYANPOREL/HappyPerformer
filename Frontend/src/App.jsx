720135589333007d4aa4da2f66481695a20e413d
anuja-sakulkar2005 <anujasakulkar.mitwpu@gmail.com>
2024-04-19 23:40:18 +0530

    Co-authored-by: Prathmesh Takalkar <Ptmesh@users.noreply.github.com> Co-authored-by: GODS3 <GODS3@users.noreply.github.com>

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,11 @@
+import PriceTag from "./Components/Software Components/PriceTag";
 import MeetTheTeam from "./Pages/Landing Page/MeetTheTeam";
+import Pricing from "./Pages/Software/Pricing";
 function App() {
   return (
     <>
-      <MeetTheTeam />
+      <Pricing/>
+
     </>
   );
 }
