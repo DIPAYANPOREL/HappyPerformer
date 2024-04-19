0400a6e36b1bab44b17f2dbce4d659ffe05654f6
Prathmesh Takalkar <takalkarprathmesh@gmail.com>
2024-04-19 20:35:22 +0530

    v 1.1

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,8 @@
-
 import MeetTheTeam from "./Pages/Landing Page/MeetTheTeam";
 function App() {
   return (
     <>
       <MeetTheTeam />
-      {/* <Login /> */}
-
-import Login from "./Pages/Software/Login";
-import Register from "./Pages/Software/Register.jsx";
-
-
-function App() {
-  return (
-    <>
-      <Login />
-      {/* <Register /> */}
-
     </>
   );
 }
