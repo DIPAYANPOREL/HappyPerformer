844597091a5337ee2640064f615467de855b43ff
Priyank Jain <priyankjohn67@gmail.com>
2024-04-24 20:28:53 +0530

    attendance page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Landing Page/Home";
 import MeetTheTeam from "./Pages/Landing Page/MeetTheTeam";
 import Dashboard from "./Pages/Software/DashBoard/Dashboard";
+import Attendance from "./Pages/Software/Attendance/Attendance";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/meet-the-team" element={<MeetTheTeam />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/attendance" element={<Attendance />} />
         </Routes>
     </>
   );
