1ed11e2c49b60b9b6ad85853c09953f6659179c4
Priyank Jain <priyankjohn67@gmail.com>
2024-05-20 16:52:05 +0530

    edit course axios logic

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,11 +6,13 @@ import Home from "./Pages/Landing Page/Home";
 import MeetTheTeam from "./Pages/Landing Page/MeetTheTeam";
 import PrivacyPolicy from "./Pages/Landing Page/PrivacyPolicy";
 import TermsAndConditions from "./Pages/Landing Page/TermsAndConditions";
-
+import Update_Delete_Media from "./Pages/Software/Courses/Update_Delete_Media";
+import Update_Media from "./Pages/Software/Courses/Update_Media";
 // Management
 // BankTransfer
 import BankTransferDetails from "./Pages/Management/BankTransfers/BankTransferDetails";
 import BankTransferPayout from "./Pages/Management/BankTransfers/BankTransferPayout";
+import EmployeeDetails from "./Pages/Management/Attendance Management/EmployeeDetails"
 
 function App() {
   return (
@@ -28,6 +30,9 @@ function App() {
         {/* BankTransfers */}
         <Route path="/banktransferdetails" element={<BankTransferDetails />} />
         <Route path="/banktransferpayout" element={<BankTransferPayout />} />
+        <Route path="/UpdateMedia/:course_id" element={<Update_Media />} />
+        <Route path="/updatedeletemedia/" element={<Update_Delete_Media />} />
+        <Route path="/employeedetails/" element={<EmployeeDetails />} />
       </Routes>
     </>
   );
